715507c253a72e9f2dc6c9e80c70f7b725af6f3a
FATHIMA <alithahir80@gmail.com>
2024-06-07 06:55:01 +0400

    code change v1.0

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print('thahir')
+    print(1+1+1)
+    print((23+32+64)/3)
+    X = 1
+    Y = 2
+    print(Y)
+    print(X)
+    a=int(4.7)
+    b=float(4)
+    print(type(a))
+    print(type(b))
+    print(a)
+    print(b)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
